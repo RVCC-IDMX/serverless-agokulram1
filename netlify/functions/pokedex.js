0a77a257314b3b93579ca1303929842438a6c62f
Gokul Ram <52551290+agokulram1@users.noreply.github.com>
2023-04-25 20:01:03 -0400

    finished assignment

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,8 +1,8 @@
-exports.handler = async function(){
+    exports.handler = async function(event, context){
     const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
     const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
-
+    console.log(event)
     const response = await fetch(POKE_API)
     const data = await response.json()
     console.log(data)
