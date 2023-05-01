f57b77dba3bf97742911dea7316f9ef671ed4c95
Cynthia Teeters <cynthiateeters@gmail.com>
2023-05-01 17:25:03 -0400

    Get Pokedex buttons working

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,8 +1,11 @@
-    exports.handler = async function(event, context){
-    const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
-    console.log(event)
+exports.handler = async function (event, context) {
+
+
+    const eventBody = JSON.parse(event.body);
+    const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`
+    //console.log(event)
     const response = await fetch(POKE_API)
     const data = await response.json()
     console.log(data)
